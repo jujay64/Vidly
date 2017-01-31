f3767b61994b160b6069bfe5a4e9c5f40967c886
Julien Inchausti <julien.inchausti@supinfo.com>
2017-01-31 12:56:51 +0900

    Exercise 4 : Designed a RESTful WebAPI to process CRUD operations on movies & customers, using ASP.Net WebAPI framework, DTOs & AutoMapper

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CUSTOMERS
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c=>c.Id,opt=>opt.Ignore());
+
+            //MOVIES
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore()); ;
+            Mapper.CreateMap<Movie, MovieDto>();
+        }
+    }
+}
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleasedDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int Stock { get; set; }
+    }
+}
